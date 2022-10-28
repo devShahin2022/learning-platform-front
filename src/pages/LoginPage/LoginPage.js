8d5ee3d03ddf5d082739fe8f56ec015ebff94820
Md Shahin Alam <108648261+devShahin2022@users.noreply.github.com>
2022-10-28 21:02:15 +0600

    Update LoginPage.js

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,18 +1,13 @@
-import React, { useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from 'react';
+
 import ContactUs from '../../components/ContactUs/ContactUs';
 import DefaultBanner from '../../components/Default-banner/DefaultBanner';
 import Footer from '../../components/Footer/Footer';
 import LoginForm from '../../components/LoginForm/LoginForm';
 import Navabar from '../../components/Navabar/Navabar';
-import { AuthContextInfo } from '../../Context/AuthContext';
 
 const LoginPage = () => {
-    const {user} = useContext(AuthContextInfo);
-    const navigate = useNavigate();
-    if(user && user.uid ){
-        return navigate('/');
-    }
+
     return (
         <>
             <Navabar></Navabar>
